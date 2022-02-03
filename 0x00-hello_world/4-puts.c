8773d51a860ead5686348789bfdd777e73dd933b
hoodrich-fern <fernan17893@gmail.com>
2022-02-03 17:52:20 +0000

    using_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
 /* This is for putting */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
