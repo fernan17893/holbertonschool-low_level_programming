7032c2bfa1b39e41ec4bc89effd13ec986080784
hoodrich-fern <fernan17893@gmail.com>
2022-02-03 18:14:04 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main- We are printing with printf
+* Return: it is 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
