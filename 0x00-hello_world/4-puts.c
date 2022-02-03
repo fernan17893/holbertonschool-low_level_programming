72c0062f7738727693872397e9475cc774e584fc
hoodrich-fern <fernan17893@gmail.com>
2022-02-03 18:00:09 +0000

    using_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
-
-/* This is for putting */
-int main(void)
+/**
+* puts- prints exact quote to output
+*/
+int puts(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
