a7704a24ce68168681048abc1c9c43bb4bc550ee
hoodrich-fern <fernan17893@gmail.com>
2022-02-04 18:53:10 +0000

    lastdigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	}
 	else if (n == 0)
 {
-	printf("The last digit of %d is %d and is 0\n", n, n = 0);
+	printf("The last digit of %d is %d and is 0\n", n, n == 0);
 	}
 	else if (n < 6)
 {
