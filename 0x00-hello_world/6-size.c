731647520a66004b63e278790adff19404f9bdfa
hoodrich-fern <fernan17893@gmail.com>
2022-02-04 13:18:02 +0000

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main-here we will print the size of the system types
 * Return: 0
@@ -7,19 +6,6 @@
 int main(void)
 {
 
-char charType; /* char charType- is the value of the char variable */
-int intType; /* is the value of the int variable */
-long int longintType; /* is the value of the long int variable */
-long long int longlongintType; /* is the value of that variable */
-float floatType; /* is the value of float */
-
-/**
-* sizeof- evaluate shows the size of each variable computed
-* @byte(s)\n"- byte(s) and add a new line
-*
-*Description: This is cool it shows the numbers.
-* Return: 0
-*/
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
