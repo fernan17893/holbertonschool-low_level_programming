6cd1105d35cf29556e77e9ce0be64bfe7d0a0ab2
hoodrich-fern <fernan17893@gmail.com>
2022-02-04 13:21:03 +0000

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
 int main(void)
 {
 
-printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
-printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(a));
+printf("Size of an int: %zu byte(s)\n", sizeof(b));
+printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
 
 return (0);
 }
