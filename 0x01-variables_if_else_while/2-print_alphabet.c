05183b62418c421a7b551b0af0d74e875ad004dc
hoodrich-fern <fernan17893@gmail.com>
2022-02-04 19:52:58 +0000

    lowercasefor

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main- info
+* Return: 0
+*/
+
+int main(void)
+{
+
+/** Get the char*/
+char ch;
+	for (ch ='a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+	putchar('\n');
+}
+	return (0);
+}
