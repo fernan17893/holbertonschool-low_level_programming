af050dae78d309cc8eb4a576fd833c4eb64dae14
hoodrich-fern <fernan17893@gmail.com>
2022-02-04 23:00:58 +0000

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main- info
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+/* print lower case */
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+{
+	putchar(ch);
+	}
+/* print upper case */
+	for (ch = 'A'; ch <= 'Z'; ch++)
+
+{
+	putchar(ch);
+	}
+{
+		putchar('\n');
+	}
+	return (0);
+	}
