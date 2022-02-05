6da5d0eb78ac3f836ca96dc2cbe29be1d6613ec9
hoodrich-fern <fernan17893@gmail.com>
2022-02-05 00:59:18 +0000

    reversealpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- info
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+
+{
+		putchar(ch);
+	}
+{
+		putchar('\n');
+	}
+	return (0);
+	}
