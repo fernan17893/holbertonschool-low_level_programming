36d658fd1fe44c6aee7eec7248eb082c1b34c88c
hoodrich-fern <fernan17893@gmail.com>
2022-02-05 01:26:47 +0000

    print16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main- Print all single digit numbers from 0-9
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+
+{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+
+{
+		putchar(ch);
+	}
+{
+		putchar('\n');
+	}
+	return (0);
+	}
