6755623eedbd4310580f54282048f4d3d2eeef75
hoodrich-fern <fernan17893@gmail.com>
2022-02-08 03:52:26 +0000

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+
+/**
+* main - prints exact quote to output
+* Return: 0
+*/
+int main(void)
+{
+puts("and that piece of art is useful\" -Dora Korpar, 2015-10-19");
+return (0);
+}
