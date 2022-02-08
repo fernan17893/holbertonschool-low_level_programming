b572c0f8c074cbc462031e6abce5ffa7858db885
hoodrich-fern <fernan17893@gmail.com>
2022-02-08 19:05:12 +0000

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
-* main- main is my custom made header with commands special for this project
-* Return: is always 0
+* print_alphabet - prints alphabet in lowercase
+*  Return: is always 0
 */
 
-void print_alphabet(void)
- /** print_alphabet - prints alphabet in lowercase
- */
+void print_alphabet(void) 
+
 
 {
 
