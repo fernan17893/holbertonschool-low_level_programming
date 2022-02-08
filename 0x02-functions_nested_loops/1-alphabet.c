9193e514bcd94935c0fde21440d404379c4c23c1
hoodrich-fern <fernan17893@gmail.com>
2022-02-08 18:59:24 +0000

    printaplhalower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main- main is my custom made header with commands special for this project
+* Return: is always 0
+*/
+
+void print_alphabet(void)
+
+{
+
+	char ap;
+
+	for (ap = 'a'; ap <= 'z'; ap++)
+
+	
+	_putchar(ap);
+	
+
+	_putchar('\n');
+
+	}
