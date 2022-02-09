37a08d3230bbeb448d4a837818c9fe5fe8d09f00
hoodrich-fern <fernan17893@gmail.com>
2022-02-09 02:47:29 +0000

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
 * _islower - checks for lower case characters
 *  Return: is 1 if lowercase, 0 otherwise
-* @c - Character being checked
+* @c:Character being checked
 */
 
 int _islower(int c)
