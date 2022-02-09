0827306865575d95fd55fe172eedab898df4ed4b
hoodrich-fern <fernan17893@gmail.com>
2022-02-09 02:13:13 +0000

    10timesloweralphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints alphabet in lowercase
+*  Return: is always 0
+*/
+
+void print_alphabet_x10(void)
+
+{
+	int ch = 0;
+	char ap;
+
+	while(ch++ <= 9)
+	{	
+	for (ap = 'a'; ap <= 'z'; ap++)
+
+	_putchar(ap);
+
+	_putchar('\n');
+	}
+}
