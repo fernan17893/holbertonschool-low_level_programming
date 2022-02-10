3a9e32d570191a7e73166b811f15aceb7eb0923c
hoodrich-fern <fernan17893@gmail.com>
2022-02-10 17:12:24 +0000

    multiple 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,17 +1,21 @@
 #include "main.h"
 /**
-* 
+* times_table- shows multiples of 9
+* @a- 9
+* @b- 9
+* @num- multiplication of a and b
 */
 
 void times_table(void)
+	
+
 {
 	int a;
 	int b;
 	int num;
-	
 
 	for (a = 0; a <= 9; a++)
-{
+	{
 	for (b = 0 ; b <= 9; b++)
 	{
 	num = (a * b);
@@ -25,19 +29,19 @@ void times_table(void)
 	if (num >= 10)
 		{
 		_putchar ((num / 10) + '0');
-		_putchar((num % 10)+ '0');
+		_putchar((num % 10) + '0');
 		}
 
-	else if(num < 10 && num != 0)
+	else if(num < 10 && b != 0)
 		{
 		_putchar(' ');
 		_putchar((num + '0'));
 		}
 	else 
 		{
-		_putchar((num % 10)+ '0');
+		_putchar((num % 10) + '0');
 		}
 	}
 	_putchar('\n');
-}
+	}
 }
