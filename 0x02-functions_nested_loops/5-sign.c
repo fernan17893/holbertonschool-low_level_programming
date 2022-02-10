fb0767cb7260e501641aaf387d5e96c87616f63a
hoodrich-fern <fernan17893@gmail.com>
2022-02-10 01:25:56 +0000

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _isalpha - checks for alphabetic character
+*  Return: is 1 if alphabetic character, 0 otherwise
+* @c:Character being checked
+*/
+
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+{
+	_putchar('+');
+
+	return (1);
+	}
+	else if ((n = 0))
+{
+	_putchar('0');
+
+	return (0);
+	}
+	else 
+{
+	_putchar('-');
+
+	return (-1);
+	}
+}
