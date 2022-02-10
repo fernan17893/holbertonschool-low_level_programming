8a96850fd110126db2e50dd7dba189ae8a770fef
hoodrich-fern <fernan17893@gmail.com>
2022-02-10 17:03:52 +0000

    multiple 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+* 
+*/
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int num;
+	
+
+	for (a = 0; a <= 9; a++)
+{
+	for (b = 0 ; b <= 9; b++)
+	{
+	num = (a * b);
+
+	 if (b != 0)
+		{
+		  _putchar(',');
+        	 _putchar(' ');
+		}
+
+	if (num >= 10)
+		{
+		_putchar ((num / 10) + '0');
+		_putchar((num % 10)+ '0');
+		}
+
+	else if(num < 10 && num != 0)
+		{
+		_putchar(' ');
+		_putchar((num + '0'));
+		}
+	else 
+		{
+		_putchar((num % 10)+ '0');
+		}
+	}
+	_putchar('\n');
+}
+}
