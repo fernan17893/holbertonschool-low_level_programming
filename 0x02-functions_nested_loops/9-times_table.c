7a8fd650dc5b3ee33c284ab3f45fa37003fdfda8
hoodrich-fern <fernan17893@gmail.com>
2022-02-10 17:19:19 +0000

    multiple 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@
 */
 
 void times_table(void)
-	
+
 
 {
 	int a;
@@ -20,24 +20,24 @@ void times_table(void)
 	{
 	num = (a * b);
 
-	 if (b != 0)
-		{
-		  _putchar(',');
-        	 _putchar(' ');
-		}
+	if (b != 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
 
 	if (num >= 10)
-		{
+	{
 		_putchar ((num / 10) + '0');
 		_putchar((num % 10) + '0');
-		}
+	}
 
-	else if(num < 10 && b != 0)
+	else if (num < 10 && b != 0)
 		{
 		_putchar(' ');
 		_putchar((num + '0'));
 		}
-	else 
+	else
 		{
 		_putchar((num % 10) + '0');
 		}
