d29ede4a2f8eb596d88877da1fbd344bcd83e65d
hoodrich-fern <fernan17893@gmail.com>
2022-02-15 18:09:49 +0000

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*
+*/
+
+void print_rev(char *s)
+
+{
+	int a = 0;
+
+
+	while (s[a] != '\0')	
+	{
+	a++;
+	}
+	for(a -= -1;  a >= 0;  a--)
+	{
+	_putchar(s[a]);
+	}
+	
+	_putchar('\n');
+
+}
+
+
