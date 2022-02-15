3d2a29a3c8086169c35f911934828554b86a08c8
hoodrich-fern <fernan17893@gmail.com>
2022-02-15 03:26:18 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line- draws a straight line
+* n: number of characters
+*
+*/
+
+void print_line(int n)
+{
+
+int l;
+
+	if (n > 0)
+	{
+	for (l = 0; l < n; l++)
+	_putchar('_');
+	}
+	_putchar('\n');
+}
