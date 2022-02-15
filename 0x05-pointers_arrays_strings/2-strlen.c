60a2f02c476c68e46962dbadb640ade7b786027f
hoodrich-fern <fernan17893@gmail.com>
2022-02-15 00:44:01 +0000

    string return

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - returns the lenght of a string
+* @s: String being counted
+*
+* Return: string lenght
+*/
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	for (; *s != '\0'; s++)
+	{
+	x++
+	}
+
+	return (x);
+}
