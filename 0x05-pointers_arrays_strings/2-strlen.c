81f75969940b2719570d70c2da1a8b1f24820e84
hoodrich-fern <fernan17893@gmail.com>
2022-02-15 00:52:26 +0000

    string return

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
-* _strlen - returns the lenght of a string
+* _strlen - Returns the lenght of a string
 * @s: String being counted
-*
-* Return: string lenght
+* 
 */
 
 int _strlen(char *s)
 {
 	int x = 0;
 
-	for (; *s != '\0'; s++)
+	for (; *s != '\0' ; s++)
 	{
-	x++
+
+	x++;
 	}
 
 	return (x);
