0b4bb64c5494d39ac0ed9def81678b5b2e389819
hoodrich-fern <fernan17893@gmail.com>
2022-02-23 17:23:56 +0000

    puts string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+* _puts_recursion- prints a string
+* @s: string to be printed
+* Return: void
+*/
+void _puts_recursion(char *s)
+{
+
+	int i = 0;
+
+	if (s[i] != 0)
+	{
+	_putchar(s[i]);
+	_puts_recursion(&s[i] + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+
+}
