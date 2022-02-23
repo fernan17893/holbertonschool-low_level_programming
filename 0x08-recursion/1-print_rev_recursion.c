4c5b219e67c8a24be3081fc39eeda4b11202b844
hoodrich-fern <fernan17893@gmail.com>
2022-02-23 17:39:00 +0000

    reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion- prints a string in reverse
+* @s: string being copied
+* Return: void
+*/
+
+void _print_rev_recursion(char *s)
+{
+
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+	_print_rev_recursion(&s[i] + 1);
+	_putchar(s[i]);
+	}
+
+
+}
