4040af123004bcd633a1e55f98b5f4ec5e3873c8
hoodrich-fern <fernan17893@gmail.com>
2022-03-03 03:55:05 +0000

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main- Multiply two numbers
+* @argc: count of arguments
+* @argv: values of arguments
+* Return: 1 if error
+*/
+
+int main(int argc, char *argv[])
+{
+
+	int x = 0, y = 0;
+
+
+	if(argc == 3)
+{
+
+	{
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
+	}
+
+
+}
+
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	return (0);
+}
