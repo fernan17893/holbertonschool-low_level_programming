eeddb52439311a64e1ba66d0f7f9045885de52c7
hoodrich-fern <fernan17893@gmail.com>
2022-03-05 03:19:00 +0000

    string dupe

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup - function that returns a pointer to a new space in mem
+* @str: string given to duplicate
+* Return: NULL if string null, pointer if string duplicated
+*
+*
+*/
+
+char *_strdup(char *str)
+{
+
+	char *s;
+	int i, j = 0;
+
+	if (str == NULL)
+	return (NULL);
+
+
+	for (j = 0; str[j] != '\0'; j++)
+
+	s = malloc(j * sizeof(char) + 1);
+	if (s == NULL)
+	return (NULL);
+
+	for (i = 0; i < j; i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,2 +1,30 @@
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+
+
+
+
+
+
+
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn( char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int sqr_check(int n, int num);
+
