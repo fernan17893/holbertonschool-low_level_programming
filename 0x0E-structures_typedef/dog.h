6290bd546efdc0b2e22bca07f759c4939d7afaca
hoodrich-fern <fernan17893@gmail.com>
2022-03-07 14:36:46 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+* struct dog - struct
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of the dog
+*/
+
 
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+
+#endif /* GRANDPARENT_H */
