258df5ac32e350b2ae13675c889e56ca05bd84e5
hoodrich-fern <fernan17893@gmail.com>
2022-03-12 01:59:23 +0000

    calc

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,34 +1,52 @@
-
 #include "3-calc.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * op_add- adds operatives
- * op_sub- subtracts operatives
- * op_mul- multiplies operatives
- * op_div- divides operatives
- * op_mod- modular of operatives
- * @a: operative 1
- * @b: operative 2
- * Return: result of operations
+ *op_add- adds two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: addition
  */
-
-
 
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+
+/**
+* op_sub- subtracts two integers
+* @a: integer 1
+* @b: integer 2
+* Return: subtraction
+*/
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+
+/**
+* op_mul- multiplies two integers
+* @a: integer 1
+* @b: integer 2
+* Return: multiplication
+*/
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
+
+/**
+* op_div- division of two integers
+* @a: integer 1
+* @b: integer 2
+* Return: division
+*/
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -38,6 +56,13 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+
+/**
+* op_mod- modular division of two integers
+* @a: integer 1
+* @b: integer 2
+* Return: modular
+*/
 int op_mod(int a, int b)
 {
 	if (b == 0)
