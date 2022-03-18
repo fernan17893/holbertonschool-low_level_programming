b85b2c774a83a7f7b5b7f4c2e038fd3076ce8a48
hoodrich-fern <fernan17893@gmail.com>
2022-03-18 01:10:27 +0000

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
+/**
+* cap_string- Capitalizes all words in a string
+* @p: pointer to string
+* Return: string
+*
+*/
+
 char *cap_string(char *p)
 {
 
 	int i = 0;
-	
 
 	for (i = 0; p[i] != '\0'; i++)
-	{ 
-	
-	
+	{
+
+
 	if (p[i] >= 'a' && p[i] <= 'z')
 	{
 	if ((p[i - 1] >= 0 && p[i - 1] <= 47) || (p[i - 1] >= 58 && p[i - 1] <= 63))
@@ -20,11 +26,11 @@ char *cap_string(char *p)
 		}
 	}
 
-	else if (p[i - 1] >= 123 || p[i - 1] == 125) 
+	else if (p[i - 1] >= 123 || p[i - 1] == 125)
 	{
 		p[i] = p[i] - 32;
 	}
 	}
-	}	
+	}
 	return (p);
 }
