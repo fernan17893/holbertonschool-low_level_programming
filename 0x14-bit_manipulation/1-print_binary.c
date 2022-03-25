66024856049ba6cc2de80d53b49000171422c959
hoodrich-fern <fernan17893@gmail.com>
2022-03-25 15:46:34 +0000

    binary rep

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_binary- print binary representation of a number
+* @n: number
+* Return: 0
+*
+*
+*/
+
+void print_binary(unsigned long int n)
+{
+
+	if (n > 1)
+	print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
