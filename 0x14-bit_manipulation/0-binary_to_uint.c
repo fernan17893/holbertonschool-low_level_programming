e2fa943ea31e97951ec256386d72d18b84ee3603
hoodrich-fern <fernan17893@gmail.com>
2022-03-25 14:32:04 +0000

    first commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* binary_to_uint- converts binary number to unsigned int
+* @b: string of unsigned int
+* Return: 0
+*
+*
+*
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	unsigned int val = 0;
+	int i = 0;
+	
+	if (b == NULL)
+		return (0);
+
+	while (b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i]-'0';
+		i++;
+	}
+	return (val);
+
+	if (b[i] != '0' || b[i] != '1')
+		return (0);
+}
