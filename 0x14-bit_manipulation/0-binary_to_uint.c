cde7c37e0ea2ca0463de954abacabc556e669e29
hoodrich-fern <fernan17893@gmail.com>
2022-03-25 14:50:30 +0000

    betty fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,18 +15,15 @@ unsigned int binary_to_uint(const char *b)
 
 	unsigned int val = 0;
 	int i = 0;
-	
-	if (b == NULL)
+
+	if (b == NULL || *b == '\0')
 		return (0);
 
 	while (b[i] == '0' || b[i] == '1')
 	{
 		val <<= 1;
-		val += b[i]-'0';
+		val += b[i] - '0';
 		i++;
 	}
 	return (val);
-
-	if (b[i] != '0' || b[i] != '1')
-		return (0);
 }
