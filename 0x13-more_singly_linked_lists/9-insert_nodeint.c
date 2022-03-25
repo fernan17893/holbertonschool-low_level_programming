5e9d69e7870f5b86e6c28758b7abfcc51b561212
hoodrich-fern <fernan17893@gmail.com>
2022-03-25 00:09:39 +0000

    insert node edge case revive

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listint_t *last = *head;
 	listint_t *new;
+	unsigned int node;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
@@ -25,14 +26,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	new->n = n;
-	new->next = NULL;
 
-	idx--;
-	while (idx != 0)
+	if (idx == 0)
 	{
-		last = last->next;
-		idx--;
+		new->next = last;
+		*head = new;
+		return (new);
 	}
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+
+		if (last == NULL || last->next == NULL)
+			return (NULL);
+
+		last = last->next;
+	}
+
 	new->next = last->next;
 	last->next = new;
 
