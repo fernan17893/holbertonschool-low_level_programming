5269e85143beb870effc5ffa05ed2d512931acd7
hoodrich-fern <fernan17893@gmail.com>
2022-03-29 18:10:56 +0000

    create fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
+		{	
 			;
+		}
 	}		
 	w = write(o, text_content, len);
 		if (w == -1)
