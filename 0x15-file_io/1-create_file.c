23dc52f0b4b04d7ec69f05c2b3451d222eba876d
hoodrich-fern <fernan17893@gmail.com>
2022-03-29 17:58:02 +0000

    create fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,25 +4,6 @@
 
 
 /**
-*_strlen - Returns the lenght of a string
-*@s: String being counted
-*Return: string lenght
-*/
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s++)
-	{
-	len++;
-	}
-
-	return (len);
-}
-
-
-/**
 * create_file- creates a file
 * @filename: name of the file
 * @text_content: string to write to the file
@@ -35,21 +16,25 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 
-	int o, w;
+	int o, w, len;
 
 
 	if (filename == NULL)
 		return (-1);
 
+
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (o == -1)
 		return (0);
-
-	w = write(o, text_content, _strlen(text_content));
+	if (text_content == NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+		w = write(o, text_content, len);
+	
 	if (w == -1)
 		return (-1);
+	}
 
-
-	close(o);
-	return (1);
+		close(o);
+		return (1);
 }
