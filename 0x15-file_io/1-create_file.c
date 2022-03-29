038cde363c9a56f077e857d36a1a0dfd7cd011ea
hoodrich-fern <fernan17893@gmail.com>
2022-03-29 18:18:29 +0000

    create fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,8 +9,6 @@
 * @text_content: string to write to the file
 * Return: 1 if success, -1 if failure
 *
-*
-*
 */
 
 int create_file(const char *filename, char *text_content)
@@ -18,10 +16,8 @@ int create_file(const char *filename, char *text_content)
 
 	int o, w, len;
 
-
 	if (filename == NULL)
 		return (-1);
-
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (o == -1)
@@ -30,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
-		{	
+		{
 			;
 		}
 	}		
