4e0add6503592a832522752c10b883d8f5c37961
hoodrich-fern <fernan17893@gmail.com>
2022-05-31 02:26:13 +0000

    task 2 3 and 4

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+
+
+
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+	hash_node_t *new_node = NULL;
+	unsigned long int index;
+
+	if (key == NULL || strcmp(key, "") == 0 || ht == NULL)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL)
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->value = strdup(value);
+			return (1);
+		}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+
+}
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_get - function that retrieves a value from a key
+* @ht: hash table
+* @key: key 
+*
+* Return: Value associated with element or Null if no key found
+*
+*
+*/
+
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp = NULL;
+	unsigned long int  index;
+
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
