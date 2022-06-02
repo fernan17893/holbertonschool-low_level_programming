03a28fa6dc2a4d747e58b10b4b2aa577d049cb34
hoodrich-fern <fernan17893@gmail.com>
2022-06-02 18:23:17 +0000

    task 5,6 and header

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,18 +1,15 @@
 #include "hash_tables.h"
 
 /**
-*
-* hash_table_print - prints hash table
-* @ht: hash table
-*
-*
-*
-*/
+ * hash_table_print - prints hash table
+ * @ht: hash table
+ *
+ */
 
 void hash_table_print(const hash_table_t *ht)
 {
 
-	
+
 	unsigned long int i = 0, j = 0;
 	hash_node_t *tmp = NULL;
 
